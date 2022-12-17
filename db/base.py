cafd437e372c0681ed40e8531830ed99fc5f9d8d
Pelykh-Ilya <pelykh.ilya@gmail.com>
2022-12-17 16:09:59 +0200

    change is base

--- db/base.py
+++ db/base.py
@@ -3,8 +3,7 @@ from sqlalchemy import create_engine, MetaData
 
 from core.config import DATABASE_URL
 
-EE_DATABASE_URL="postgresql://root:root@localhost:32700/employment_exchange"
-database = Database(EE_DATABASE_URL)#DATABASE_URL
+database = Database(DATABASE_URL)
 metadata = MetaData()
 engine = create_engine(
     DATABASE_URL
